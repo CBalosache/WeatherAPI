dc33e077c5fb793a27e8eb5d4c8ebf34f37f045f
Cosmin Balosache <CBalosache@spartaglobal.com>
2019-10-09 13:55:18 +0100

    Reworked model

--- OpenWeatherMapAPITest/Open_Weather_Map_API_Service/Data_Handling/OpenWeatherMapApiModel.cs
+++ OpenWeatherMapAPITest/Open_Weather_Map_API_Service/Data_Handling/OpenWeatherMapApiModel.cs
@@ -6,29 +6,29 @@ using System.Threading.Tasks;
 
 namespace OpenWeatherMapAPITest.Open_Weather_Map_API_Service.Data_Handling
 {
-    //public class hdh
-    //{
-    //    public string DT { get; set; }
-    //    public string dt_txt { get; set; }
-    //}
     public class WeatherList
     {
         public int dt { get; set; }
         public string dt_txt { get; set; }
         public Main main { get; set; }
         public Weather[] weather { get; set; }
-        
+        public Clouds clouds { get; set; }
+        public Wind wind { get; set; }
+        public Rain rain { get; set; }
+        public Sys sys { get; set; }
+        public City city { get; set; }
+        public Coord coord { get; set; }
     }
     public class Main
     {
-        public double Temp { get; set; }
-        public double Temp_Min { get; set; }
-        public double Temp_Max { get; set; }
-        public double Pressure { get; set; }
-        public double Sea_Level { get; set; }
-        public double Grnd_Level { get; set; }
-        public int Humidity { get; set; }
-        public double Temp_kf { get; set; }
+        public double temp { get; set; }
+        public double temp_Min { get; set; }
+        public double temp_Max { get; set; }
+        public double pressure { get; set; }
+        public double sea_Level { get; set; }
+        public double grnd_Level { get; set; }
+        public int humidity { get; set; }
+        public double temp_kf { get; set; }
     }
     public class Weather
     {
@@ -39,42 +39,42 @@ namespace OpenWeatherMapAPITest.Open_Weather_Map_API_Service.Data_Handling
     }
     public class Clouds
     {
-        public int All { get; set; }
-        public double Deg { get; set; }
+        public int all { get; set; }
     }
     public class Wind
     {
-        public double Speed { get; set; }
+        public double speed { get; set; }
+        public double deg { get; set; }
     }
     public class Rain
     {
-        public decimal Threeh { get; set; }
+        public double threeh { get; set; }
     }
     public class Sys
     {
-        public char Pod { get; set; }
+        public char pod { get; set; }
     }
     public class City
     {
-        public string ID { get; set; }
-        public string Name { get; set; }
-        public string Country { get; set; }
-        public int Population { get; set; }
-        public int Timezone { get; set; }
-        public int Sunrise { get; set; }
-        public int Sunset { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string country { get; set; }
+        public int population { get; set; }
+        public int timezone { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
     }
     public class Coord
     {
-        public double Lat { get; set; }
-        public double Lon { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
     }
     public class OpenWheatherMapRoot
     {
         public string cod { get; set; }
         public string message { get; set; }
         public string cnt { get; set; }
-        //public hdh list { get; set; }
+        public WeatherList weatherList { get; set; }
         public City city { get; set; }
     }
 }
